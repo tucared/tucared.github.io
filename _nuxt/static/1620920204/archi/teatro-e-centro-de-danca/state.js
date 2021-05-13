3e086667e94b30fff67e95f52af7cd7c9a08e005
Max <max.perdrigeat@gmail.com>
2021-05-13 17:47:44 +0200

    Updates

--- _nuxt/static/1620920204/archi/teatro-e-centro-de-danca/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620920204",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchi\u002Fteatro-e-centro-de-danca",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"98122f8b"}}}}(null));
