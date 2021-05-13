28f24ab69d27901bfe962355a8478a7f01fdfeb8
Max <max.perdrigeat@gmail.com>
2021-05-13 16:43:35 +0200

    Added font and landing styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div id="landing">
-    <h1>Clarissa Aburachid Rocha</h1>
-    <NuxtLink to="/archi">Architecture</NuxtLink>
-    <NuxtLink to="/photo">Photographie</NuxtLink>
-  </div>
+  <section class="centered">
+    <h1 id="name">Clarissa Aburachid Rocha</h1>
+    <div id="landing-links-container">
+      <div class="landing-link">
+        <NuxtLink to="/archi">Architecture</NuxtLink>
+      </div>
+      <div class="landing-link">
+        <NuxtLink to="/photo">Photographie</NuxtLink>
+      </div>
+    </div>
+  </section>
 </template>
