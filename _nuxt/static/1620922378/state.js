0485b201e8abf6f43995a6a7e659027dfcfbe06e
Max <max.perdrigeat@gmail.com>
2021-05-14 10:00:10 +0200

    Updates

--- _nuxt/static/1620922378/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620922378",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e0cc0fd0"}}}}(null,"\u002F"));
