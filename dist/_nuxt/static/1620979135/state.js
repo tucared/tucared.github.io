24f66cd40d10dd637d4aabdbebb066c86961fbda
Max <max.perdrigeat@gmail.com>
2021-05-14 09:59:43 +0200

    Generated static

---
+++ dist/_nuxt/static/1620979135/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620979135",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"108e3269"}}}}(null,"\u002F"));
