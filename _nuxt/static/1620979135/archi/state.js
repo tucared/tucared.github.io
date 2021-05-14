60fd88e09b4a58aca9d642eb8bc4890ebb08d1dd
Max <max.perdrigeat@gmail.com>
2021-05-14 10:14:44 +0200

    Updates

--- _nuxt/static/1620979135/archi/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620979135",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"108e3269"}}}}(null));
