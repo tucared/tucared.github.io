f18917f5c77e2d5696e35b29e44828a1ccf3b95e
Tucared <max.perdrigeat@gmail.com>
2021-12-24 13:11:28 -0300

    Fixed vulnerability with glob-parent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  purge: [
+  content: [
     './components/**/*.{vue,js}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media",
   theme: {
     extend: {},
     colors: {
