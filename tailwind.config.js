c55f8876a913e5d4214d07c478393b1da1f1749b
Tucared <max.perdrigeat@gmail.com>
2021-12-28 11:57:39 -0300

    Changed tailwind config for v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  content: [
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-  ],
+  content: {
+    "files": [
+      './components/**/*.{vue,js}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}',
+    ]
+  },
   darkMode: "media",
   theme: {
     extend: {},
