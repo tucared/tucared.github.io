23704c2efe2f9bc1e1756ce7fb07a043fa0113db
Tucared <max.perdrigeat@gmail.com>
2022-01-02 16:48:36 -0300

    (ERRATUM) Renamed project list page from Interior to Projects, and removed interior from full path of project page

---
+++ pages/projects.vue
@@ -0,0 +1,52 @@
+
+<template>
+  <div
+    class="min-w-full h-full min-h-screen p-10 bg-terciary overscroll-contain"
+  >
+    <div>
+      <h1 class="text-xl p-10 font-bold justify-start">Projects</h1>
+    </div>
+    <div>
+      <ul class="columns-3xs">
+        <li
+          class="p-3 w-full break-inside-avoid"
+          v-for="project of projects"
+          :key="project.slug"
+        >
+          <NuxtLink :to="`${project.slug}`">
+            <div>
+              <img v-if="project.img" class="object-cover" :src="project.img" />
+              <h2>{{ project.title }}</h2>
+            </div>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    try {
+      // Will be used for filtering later
+      const [theme] = await $content(params.slug)
+        .where({ slug: params.theme })
+        .only(["title", "slug"])
+        .fetch();
+
+      const projects = await $content("projects", { deep: true })
+        .only(["title", "img", "slug"])
+        .sortBy("createdAt", "asc")
+        .fetch();
+
+      return { theme, projects };
+    } catch (err) {
+      error({
+        statusCode: 404,
+        message: "Theme could not be found",
+      });
+    }
+  },
+};
+</script>
